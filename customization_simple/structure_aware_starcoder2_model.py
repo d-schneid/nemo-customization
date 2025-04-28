0dd2790dc7f86484bbc6dfe133c44c4fd376eb59
d-schneid <116602991+d-schneid@users.noreply.github.com>
2025-04-28 18:00:46 +0200

    Add files via upload

---
+++ customization_simple/structure_aware_starcoder2_model.py
@@ -0,0 +1,27 @@
+from nemo.collections import llm
+from nemo.collections.llm import Starcoder2Model, Qwen2Model
+from torch import nn
+from typing import Annotated, Callable, Optional
+from nemo.lightning import OptimizerModule
+from nemo.collections.llm.utils import Config
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
+
+
+class StructureAwareStarcoder2Model(Starcoder2Model):
+
+	def __init__(
+			self,
+			config: Annotated[Optional[llm.Starcoder2Config], Config[llm.Starcoder2Config]] = None,
+			optim: Optional[OptimizerModule] = None,
+			tokenizer: Optional["TokenizerSpec"] = None,
+			model_transform: Optional[Callable[[nn.Module], nn.Module]] = None,
+	):
+		super().__init__(
+			config=config,
+			optim=optim,
+			tokenizer=tokenizer,
+			model_transform=model_transform,
+		)
